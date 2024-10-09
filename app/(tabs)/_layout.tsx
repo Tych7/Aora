3716ced18b2f9168e29c1239474267a61e199990
Tycho746 <jurjens2001@gmail.com>
2024-10-09 16:42:46 +0200

    first change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs, Redirect} from 'expo-router';
 
 import { icons } from '../../constants';
 
-const TabIcon = ({icon, color, name, focused, test}) => {
+const TabIcon = ({icon, color, name, focused}) => {
     return (
         <View>
             <Image
