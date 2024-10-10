ac0af51b18d90bfc388d27ba95ab1f3c293605e6
Tycho746 <jurjens2001@gmail.com>
2024-10-10 17:01:53 +0200

    start home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,21 @@
-import { View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 import { Tabs, Redirect} from 'expo-router';
 
 import { icons } from '../../constants';
 
 const TabIcon = ({icon, color, name, focused}) => {
     return (
-        <View>
+        <View style={styles.tab}>
             <Image
                 source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                style={{width: 24, height: 24}}
             />
+            <Text style={[{color: color}, focused ? styles.text_bold : styles.text_reqular]}>
+                {name}
+            </Text>
+
         </View>
     )
 }
@@ -16,7 +23,19 @@ const TabIcon = ({icon, color, name, focused}) => {
 const TabsLayout = () => {
   return (
     <>
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: '#FFA001',
+                tabBarInactiveTintColor: '#CDCDE0',
+                tabBarStyle:{
+                    backgroundColor: '#161622',
+                    borderTopWidth: 1,
+                    borderTopColor: '#232533',
+                    height: 84,
+                }
+            }}
+        >
             <Tabs.Screen
                 name="home"
                 options={{
@@ -32,9 +51,71 @@ const TabsLayout = () => {
                     )
                 }}
             />
+            <Tabs.Screen
+                name="bookmark"
+                options={{
+                    title: 'Bookmark',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused}) => (
+                        <TabIcon
+                            icon={icons.bookmark}
+                            color={color}
+                            name="Bookmark"
+                            focused={focused} 
+                        />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="create"
+                options={{
+                    title: 'Create',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused}) => (
+                        <TabIcon
+                            icon={icons.plus}
+                            color={color}
+                            name="Create"
+                            focused={focused} 
+                        />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused}) => (
+                        <TabIcon
+                            icon={icons.profile}
+                            color={color}
+                            name="Profile"
+                            focused={focused} 
+                        />
+                    )
+                }}
+            />
         </Tabs>
     </>
   )
 }
 
 export default TabsLayout
+
+const styles = StyleSheet.create({
+    text_reqular: {
+      fontSize: 14,
+      fontFamily: 'Poppins-Regular',
+    },
+    text_bold: {
+        fontSize: 14,
+        fontFamily: 'Poppins-SemiBold',
+      },
+    tab: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+    },
+  })
--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../constants'
+
 
 const index = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Aora!</Text>
-      <StatusBar style='auto'/>
-      <Link href="/home" style={{color: 'blue'}}>Go to home</Link>
-    </View>
+    <SafeAreaView style={styles.basis}>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View style={styles.scroll_menu}>
+          <Image style={styles.logo_image}
+            source={images.logo}
+          />
+
+          <Image style={styles.cards_image}
+            source={images.cards}
+          />
+
+          <View style={styles.text_view}>
+            <Text style={[styles.text, {color: 'white'}]}>Discover Endless Posibilities with {' '}
+              <Text style={[styles.text, {color: '#FF9C01'}]}>
+                Aora
+              </Text>
+            </Text>
+
+          <Image style={styles.logo_underline_image}
+            source={images.path}
+          />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default index
 
 const styles = StyleSheet.create({
-  container: {
+  basis: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#161622',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo_image: {
+    width: 130,
+    height: 84,
+    resizeMode: "contain",
+  },
+  logo_underline_image: {
+    width: 136,
+    height: 15,
+    position: 'absolute',
+    bottom: 3,
+    right: -32,
+    resizeMode: 'contain'
+  },
+  cards_image: {
+    maxWidth: 380,
+    width: '100%',
+    height: 300,
+    resizeMode: "contain",
+  },
+  scroll_menu: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    alignItems: 'center',
+    paddingHorizontal: 4,
+  },
+  text_view: {
+    marginTop: 5,
+  },
   text: {
-    fontSize: 40,
-    fontFamily: 'Poppins-Black',
+    fontSize: 30,
+    fontFamily: 'Poppins-SemiBold',
+    textAlign: 'center'
   },
 })
