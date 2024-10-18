8224da5a9b8ab11d5d3753cf7b7a63fe8208d462
Tych7 <tychojurjens2001@gmail.com>
2024-10-18 09:18:45 +0200

    added sign up screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -44,11 +44,11 @@ const SignIn = () => {
             containerStyles={{width: '95%', marginTop: 28}}
             isLoading={IsSubmitting}
           />
-          <View style={{justifyContent:'center', padding: 5, flexDirection: 'row'}}>
+          <View style={{justifyContent:'center', padding: 5, flexDirection: 'row', marginTop: 20}}>
             <Text style={styles.text_regular}>
               Don't have account?
             </Text>
-            <Link href="/sign-up" style={styles.text_semibold}>Sign Up</Link>
+            <Link href="/sign-up" style={styles.text_bottom}>Sign Up</Link>
           </View>
         </View>
       </ScrollView>
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     paddingHorizontal: 4,
-    marginTop: 6,
+    marginTop: 85,
     alignItems: 'center',
   },
   logo:{
@@ -92,9 +92,9 @@ const styles = StyleSheet.create({
   },
   text_bottom:{
     fontFamily: 'Poppins-SemiBold',
-    marginTop: 40,
-    fontSize: 24,
-    lineHeight: 32,
-    color: 'white',
+    fontSize: 18,
+    lineHeight: 28,
+    marginLeft: 10,
+    color: '#FF9C01',
   }
 })
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,107 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Dimensions, Image} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import { useState } from 'react'
+import { Link } from 'expo-router'
+
+import FormField from '@/components/FormField'
+import CustomButton from '@/components/CustomButton'
 
 const SignUp = () => {
+  const [form, setform] = useState({
+    username: '',
+    email: '',
+    password: '',
+  })
+  const [IsSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView style={styles.basis}>
+      <ScrollView>
+        <View style={styles.canvas}>
+          <Image style={styles.logo} source={images.logo}/>
+          <Text style={styles.text_top}>Sign up to Aora</Text>
+          <FormField
+            title="Username"
+            value={form.email}
+            handleChangeText={(e) => setform({...form,email: e})}
+            otherStyles={{marginTop: 35}}
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setform({...form,email: e})}
+            otherStyles={{marginTop: 28}}
+            keyboardType="email-adress"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setform({...form,password: e})}
+            otherStyles={{marginTop: 28}}
+          />
+          <CustomButton
+            title="Sign up"
+            handlePress={submit}
+            containerStyles={{width: '95%', marginTop: 28}}
+            isLoading={IsSubmitting}
+          />
+          <View style={{justifyContent:'center', padding: 5, flexDirection: 'row', marginTop: 20}}>
+            <Text style={styles.text_regular}>
+              Have an account already?
+            </Text>
+            <Link href="/sign-in" style={styles.text_bottom}>Sign In</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignUp
+
+const styles = StyleSheet.create({
+  basis: {
+    flex: 1,
+    backgroundColor: '#161622',
+    height: '100%', 
+  },
+  canvas: {
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: 4,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  logo:{
+    resizeMode:'contain',
+    width: 115,
+    height: 35,
+  },
+  text_regular:{
+    fontFamily: 'Poppins-Regular',
+    fontSize: 18,
+    lineHeight: 28,
+    color: '#d0ccdc',
+
+  },
+  text_top:{
+    fontFamily: 'Poppins-SemiBold',
+    marginTop: 40,
+    fontSize: 24,
+    lineHeight: 32,
+    color: 'white',
+  },
+  text_bottom:{
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 18,
+    lineHeight: 28,
+    marginLeft: 10,
+    color: '#FF9C01',
+  }
+})
